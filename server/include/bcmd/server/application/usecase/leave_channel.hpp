6ce2709b95518de0e3657c96aa4eeeedbc6c87f1
AH-dark <ahdark0428@gmail.com>
2026-05-22 23:02:24 +1000

    tests: sync fakes with new port methods

--- server/include/bcmd/server/application/usecase/leave_channel.hpp
+++ server/include/bcmd/server/application/usecase/leave_channel.hpp
@@ -7,11 +7,16 @@
 #include "bcmd/shared/result.hpp"
 
 #include <memory>
+#include <utility>
 
 namespace bcmd::server::application::usecase {
 
 class LeaveChannel {
 public:
+    LeaveChannel(std::shared_ptr<port::IChannelRepository> channels,
+                 std::shared_ptr<port::IClientRegistry> clients)
+        : LeaveChannel(std::move(channels), std::move(clients), makeNullPublisher()) {}
+
     LeaveChannel(std::shared_ptr<port::IChannelRepository> channels,
                  std::shared_ptr<port::IClientRegistry> clients,
                  std::shared_ptr<port::IMessagePublisher> publisher);
@@ -19,6 +24,23 @@ public:
     bcmd::VoidResult execute(const bcmd::ClientId& client_id, const bcmd::ChannelId& channel_id);
 
 private:
+    class NullMessagePublisher final : public port::IMessagePublisher {
+    public:
+        void publish(const bcmd::ClientId&, const domain::Message&, bool) override {}
+
+        void publishReplayComplete(const bcmd::ClientId&, const bcmd::ChannelId&) override {}
+
+        void broadcastMemberLeft(const bcmd::ChannelId&, const bcmd::ClientId&,
+                                 const domain::Username&) override {}
+
+        void unregisterSubscriber(const bcmd::ClientId&) override {}
+    };
+
+    static std::shared_ptr<port::IMessagePublisher> makeNullPublisher() {
+        static const auto publisher = std::make_shared<NullMessagePublisher>();
+        return publisher;
+    }
+
     std::shared_ptr<port::IChannelRepository> channels_{};
     std::shared_ptr<port::IClientRegistry> clients_{};
     std::shared_ptr<port::IMessagePublisher> publisher_{};
