67ecdbc38d81d0d3c3426f291e4dfd5bf44ac9ff
AH-dark <ahdark0428@gmail.com>
2026-05-22 21:41:02 +1000

    feat(domain): add last_heartbeat_at_ to ClientSession

--- server/src/domain/model/client_session.cpp
+++ server/src/domain/model/client_session.cpp
@@ -11,7 +11,14 @@ namespace bcmd::server::domain {
 ClientSession::ClientSession(bcmd::ClientId id, Username username)
     : id_(std::move(id)),
       username_(std::move(username)),
-      connected_since_(std::chrono::system_clock::now()) {}
+      connected_since_(std::chrono::system_clock::now()),
+      last_heartbeat_at_(std::chrono::steady_clock::now()) {}
+
+void ClientSession::touch() noexcept { last_heartbeat_at_ = std::chrono::steady_clock::now(); }
+
+bool ClientSession::isExpired(std::chrono::steady_clock::time_point deadline) const noexcept {
+    return last_heartbeat_at_ < deadline;
+}
 
 void ClientSession::joinChannel(const bcmd::ChannelId& channel_id) {
     joined_channels_.insert(channel_id);
