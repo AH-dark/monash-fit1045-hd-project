f85a4ed75a638526daf5d79abace0a9c64fdcd3c
AH-dark <ahdark0428@gmail.com>
2026-05-28 13:12:23 +1000

    style(client-web): apply biome formatting and lint fixes to existing code

--- client-web/src/routes/index.tsx
+++ client-web/src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-  component: () => <div>client-web (UI pending)</div>,
-})
+export const Route = createFileRoute("/")({
+	component: () => <div>client-web (UI pending)</div>,
+});
